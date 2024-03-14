6ef869a8db6a3670e148f435b76b96f24dc66abd
kmgyu <lidersy961@gmail.com>
2024-03-14 12:26:13 +0900

    page_exractor

--- examples/playwright_example.py
+++ examples/playwright_example.py
@@ -1,7 +1,11 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 import time
+import csv
 
 p = sync_playwright().start()
+base = "https://www.wanted.co.kr"
+jobs_db = []
 
 browser = p.chromium.launch(headless=False)
 
@@ -10,23 +14,23 @@ page = browser.new_page()
 # page.goto("https://google.com")
 page.goto("https://www.wanted.co.kr/jobsfeed")
 
-time.sleep(2)
+time.sleep(1)
 
 page.click("button.Aside_searchButton__Xhqq3")
 # same code down here
 # page.locator("button.Aside_searchButton__Xhqq").click()
 
-time.sleep(2)
+time.sleep(1)
 
 page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
 # you can get by label, id... etc
 
 page.keyboard.down("Enter")
 # press key enter
-time.sleep(4)
+time.sleep(1)
 
 page.click("a#search_tab_position")
-time.sleep(4)
+time.sleep(1)
 
 for i in range(5):
     page.keyboard.down("End")
@@ -37,3 +41,36 @@ for i in range(5):
 content = page.content()
 
 p.stop()
+
+soup = BeautifulSoup(content, "html.parser")
+
+jobs = soup.find_all("div", class_="JobCard_container__FqChn")
+
+for job in jobs:
+    link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+    title = job.find("strong", class_ = "JobCard_title__ddkwM").text
+    company_name = job.find("span", class_ = "JobCard_companyName__vZMqJ").text
+    # location = job.find("span", )
+    reward = job.find("span", class_="JobCard_reward__sdyHn").text
+    job_info = {
+        "title":title,
+        "company_name":company_name,
+        "reward":reward,
+        "link":link,
+    }
+    jobs_db.append(job_info)
+
+
+
+file = open("jobs.csv", mode="w", encoding="utf-8", newline="")
+writer = csv.writer(file)
+writer.writerow(
+    ["Title",
+     "Company",
+     "Location", 
+     "Reward"])
+
+for job in jobs_db:
+    writer.writerow(job.values())
+file.close()
+
---
+++ extractor/extractor.py
@@ -0,0 +1,88 @@
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import time
+import csv
+
+class page_extractor:
+    
+    def __init__(self, base, search_query, keyword):
+        self.base = base
+        self.search_query = search_query
+        self.keyword = keyword
+        
+        self.p = sync_playwright().start()
+        self.jobs_db = []
+
+        # headless True
+        self.browser = self.p.chromium.launch()
+
+        self.page = self.browser.new_page()
+
+        self.page.goto(base + search_query + keyword)
+
+    def action_wanted(self):
+        # 원티드 페이지에서 적용됨
+        self.page.click("a#search_tab_position")
+        time.sleep(0.1)
+
+        for _ in range(5):
+            self.page.keyboard.down("End")
+            # scrolling down
+            time.sleep(0.1)
+        
+    
+    def extract(self):
+        # 원티드아니면 다른거 써야됨.
+        # 사이트가 다르면 class가 다를 것이므로... 원티드 아니면 작동 안 할것..
+        self.action_wanted()
+        
+        self.content = self.page.content()
+        self.p.stop()
+
+        soup = BeautifulSoup(self.content, "html.parser")
+
+        jobs = soup.find_all("div", class_="JobCard_container__FqChn")
+
+        for job in jobs:
+            link = f"{self.base}{job.find('a')['href']}"
+            title = job.find("strong", class_ = "JobCard_title__ddkwM").text
+            company_name = job.find("span", class_ = "JobCard_companyName__vZMqJ").text
+            reward = job.find("span", class_="JobCard_reward__sdyHn").text
+            job_info = {
+                "title":title,
+                "company_name":company_name,
+                "reward":reward,
+                "link":link,
+            }
+            self.jobs_db.append(job_info)
+
+        self.file = open(f"db/{self.keyword}.csv", mode="w", encoding="utf-8", newline="")
+        self.writer = csv.writer(self.file)
+        self.writer.writerow(
+            ["Title",
+            "Company",
+            "Location", 
+            "Reward"])
+
+        for job in self.jobs_db:
+            self.writer.writerow(job.values())
+        self.file.close()
+
+
+base = "https://www.wanted.co.kr"
+# search_query + keyword to search
+search_query = "/search?query="
+
+keywords = [
+    "flutter",
+    "vue",
+    "node.js",
+    "python",
+]
+
+for keyword in keywords:
+    p_extractor = page_extractor(base=base, search_query=search_query, keyword=keyword)
+    p_extractor.extract()
+
+
+
